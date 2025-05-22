b92f04825e342fbb877f1c0e8df53b9c3e4f6778
RoNaLd82QS <ronaldpale@gmail.com>
2025-05-22 02:40:26 -0500

    Estilos_Carrito

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -6,26 +6,26 @@ using System.Diagnostics.CodeAnalysis;
 namespace Proyect_Snake_West.Models
 {
     [Table("t_producto")]
-public class Producto
-{
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
 
-    [Required]
-    [Column(TypeName = "decimal(18,2)")]
-    public decimal Price { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
 
-    [Required]
-    [StringLength(20)]
-    public string Status { get; set; } = "Activo"; // Valor por defecto
+        [Required]
+        [StringLength(20)]
+        public string Status { get; set; } = "Activo"; // Valor por defecto
 
-    [Required]
-    [Url]
-    public string ImageURL { get; set; } = string.Empty;
+        [Required]
+        [Url]
+        public string ImageURL { get; set; } = string.Empty;
 }
 }

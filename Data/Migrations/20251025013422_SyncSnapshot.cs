5482b0a2febf51e60c6ae39d9cfe198754c90a0a
Frank <jhoncorilla2014@gmail.com>
2025-10-24 22:16:38 -0500

    Agregando EF migration SyncSnapshot

---
+++ Data/Migrations/20251025013422_SyncSnapshot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyect_Snake_West.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
